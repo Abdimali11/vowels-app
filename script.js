c76e36a7d71f721a6ec3ff21a3dbd157cb71a558
Abdimalik11 <ilkacaze5522@gmail.com>
2023-07-08 12:51:51 -0700

    this is the answer of vowels-app

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 
+// Get the necessary elements from the HTML
+let inputText = document.querySelector('#input-text');
+let vowelCount = document.querySelector('#vowel-count');
+let countBtn = document.querySelector('#count-btn');
+
+ 
+
+countBtn.addEventListener('click', function()  {
+ 
+  const text = inputText.value.toLowerCase();
+
+ 
+ 
+  const vowels = ['a', 'e', 'i', 'o', 'u'];
+  const count = text.split('').filter(char => vowels.includes(char)).length;
+
+ 
+ 
+  vowelCount.textContent = count;
+});
